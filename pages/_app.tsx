ed7c4bc4b02dc5a23141d7d1e2897a289d4d1a1a
francis balimuttajjo <63359032+francisbalimuttajjo@users.noreply.github.com>
2022-10-03 09:22:31 +0300

    Update _app.tsx
    fix nprogress error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import Head from "next/head";
-import Nprogress from "nprogress";
-import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
