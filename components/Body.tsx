0849e3c1f1a672d22be31b5706987251ea2ee820
francis balimuttajjo <63359032+francisbalimuttajjo@users.noreply.github.com>
2022-10-03 09:45:28 +0300

    Update Body.tsx
    fix background color issue

--- components/Body.tsx
+++ components/Body.tsx
@@ -5,7 +5,7 @@ import DetailsSection2 from "./DetailsSection2";
 const Body: React.FC = () => {
   return (
     <div className="flex justify-end sm:pl-2  ">
-      <div className=" pb-10 w-full min-h-screen md:w-10/12 bg-gradient-to-tr from-gray-800 to-indigo-800">
+      <div className=" pb-10 w-full min-h-screen md:w-10/12 bg-gradient-to-tr from-gray-900  via-gray-800 to-indigo-900">
         <Header />
         <div className="md:-ml-[10%]  md:flex px-2 ">
           <DetailsSection1 />
