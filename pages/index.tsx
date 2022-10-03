2d6a4e25376dc015163e15ef4e510334da3c44fb
francis <fbalimuttajjo@gmail.com>
2022-10-03 09:14:50 +0300

    fix nprogress error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import NavBar from "../components/Nav";
 import Body from "../components/Body";
 import Footer from "../components/Footer";
+
 const Index = () => {
   return (
     <div className="bg-zinc-900 min-h-screen">
